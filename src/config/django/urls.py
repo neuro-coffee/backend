6e9a9a0b1ef2e50cdb5f93ab939fa910ec81e26c
Mikhail <m.sannikov@itcodegroup.ru>
2025-05-30 13:59:05 +0500

    ruff

--- src/config/django/urls.py
+++ src/config/django/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.views.generic import RedirectView
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
